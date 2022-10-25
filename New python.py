7d12c843afbd9fe68d8a1203a3acefe24558db75
onm123123 <omhellyamrut@gmail.com>
2022-10-25 10:51:51 -0600

    Create New python.py

---
+++ New python.py
@@ -0,0 +1,18 @@
+if (5>2):
+    print("I know maths")
+    #cool
+
+    x = 5
+    y = 5    
+    print(x + y) 
+    myvar= "Maths"
+    x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+print (myvar)
