3fc6233b0b909a353375174aaeff08f4744d6405
onm123123 <112973174+onm123124@users.noreply.github.com>
2023-01-06 11:42:06 -0700

    Update New python.py

--- New python.py
+++ New python.py
@@ -1,9 +1,9 @@
 weight = input("Wieght: ")
-unit = input("(K)ilograms or (L)bs: ")
-Kilograms = float(float(weight) * 2.204)
-Pounds = float(float(weight) // 2.204) 
+unit = input("Convert to (K)ilograms or (L)bs: ")
+Kilograms = float(float(weight) // 2.204)
+Pounds = float(float(weight) * 2.204) 
 if unit == "K" or unit == "k":
     print("Your weight in kilos is: " + str(Kilograms))
 
 if unit == "L" or unit == "l":
-    print("Your weight in pounds is: " + str(Pounds))
+    print("Your weight in (L)bs is: " + str(Pounds))
