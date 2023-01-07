95244a8d7df1f8586765925d4bcdae8846e88ee8
onm123123 <112973174+onm123124@users.noreply.github.com>
2023-01-07 09:43:06 -0700

    Update New_python.py

--- New_python.py
+++ New_python.py
@@ -2,4 +2,4 @@ import random
 numbers = (1,2,3,4,5,6,7,8,9,10)
 duplicatenumbers = random.randrange(1,10)
 print (duplicatenumbers, numbers)
-print (duplicatenumbers, "is your duplicated number.")
+print (duplicatenumbers, "is your duplicated number.") 
